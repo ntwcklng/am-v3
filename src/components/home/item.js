7ae21ab29b0322025b4b0a1d54d4dc217f9b95ea
ntwcklng <ntwcklng@gmail.com>
2016-11-24 16:18:26 +0100

    lint

--- src/components/home/item.js
+++ src/components/home/item.js
@@ -1,27 +1,27 @@
-import { h, Component } from 'preact';
-import style from './style';
+import { Component } from 'preact'
+import style from './style'
 
 export default class LeistungenItem extends Component {
-	render({data}) {
-		let infos = null;
-		if (data.infos) {
-			infos = <span> | Weitere Infos: <a href={data.infos[0].url} target="_blank">{data.infos[0].title}</a></span>;
-		}
-		return (
-			<li class={style.leistungenItem}>
-				<div>
-					<h1>{data.title}</h1>
-					<hr />
-					<p>
-						{data.desc.split("\n").map((item)=>{
-							return (<span>{item}<br /></span>);
-						})}
-					</p>
-					<p>
-						<strong>ab {data.price}€</strong>{infos}
-					</p>
-				</div>
-			</li>
-		);
-	}
+  render ({data}) {
+    let infos = null
+    if (data.infos) {
+      infos = <span> | Weitere Infos: <a href={data.infos[0].url} target='_blank'>{data.infos[0].title}</a></span>
+    }
+    return (
+      <li class={style.leistungenItem}>
+        <div>
+          <h1>{data.title}</h1>
+          <hr />
+          <p>
+            {data.desc.split('\n').map((item) => {
+              return (<span>{item}<br /></span>)
+            })}
+          </p>
+          <p>
+            <strong>ab {data.price}€</strong>{infos}
+          </p>
+        </div>
+      </li>
+    )
+  }
 }
